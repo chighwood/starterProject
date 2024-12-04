6381b4b3d9f88d034a9ab006c6bbe7167cf2bf4d
chighwood <hig23003@byui.edu>
2024-12-04 17:05:34 -0600

    Added Account-Model code

--- models/account-model.js
+++ models/account-model.js
@@ -4,8 +4,8 @@ const pool = require("../database/")
 async function checkExistingEmail(account_email){
     try {
       const sql = "SELECT * FROM account WHERE account_email = $1"
-      const email = await pool.query(sql, [account_email])
-      return email.rowCount
+      const email = await pool.query(sql, [account_email]);
+      return email.rowCount > 0;
     } catch (error) {
       return error.message
     }
